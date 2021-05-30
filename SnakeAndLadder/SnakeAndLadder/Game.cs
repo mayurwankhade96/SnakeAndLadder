cf10932fbc6f97b919abb88c952efd36a1344d7c
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-30 22:25:15 +0530

    [Mayur] Refactor : Game rules added

--- SnakeAndLadder/SnakeAndLadder/Game.cs
+++ SnakeAndLadder/SnakeAndLadder/Game.cs
@@ -7,12 +7,27 @@ namespace SnakeAndLadder
     class Game
     {
         public int startPosition = 0;
+        const int IS_LADDER = 1;
+        const int IS_SNAKE = 2;
         public void SnakeLadder()
         {
             Random die = new Random();
             int dieRoller = die.Next(1, 7);
+            int SnakeOrLadder = die.Next(0, 3);
 
-            Console.WriteLine("Player rolls the die and get : " + dieRoller);
+            switch (SnakeOrLadder)
+            {
+                case IS_LADDER:
+                    startPosition += dieRoller;
+                    break;
+
+                case IS_SNAKE:
+                    startPosition -= dieRoller;
+                    break;
+
+                default:                    
+                    break;
+            }            
         }
     }
 }
