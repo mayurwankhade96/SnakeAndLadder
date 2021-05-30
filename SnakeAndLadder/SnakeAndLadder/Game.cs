ae7d4d7933f6b1b9d37dedc6133ef8a56a09677d
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-31 00:59:21 +0530

    [Mayur] Refactor : Winning position added

--- SnakeAndLadder/SnakeAndLadder/Game.cs
+++ SnakeAndLadder/SnakeAndLadder/Game.cs
@@ -12,22 +12,34 @@ namespace SnakeAndLadder
         public void SnakeLadder()
         {
             Random die = new Random();
-            int dieRoller = die.Next(1, 7);
-            int SnakeOrLadder = die.Next(0, 3);
 
-            switch (SnakeOrLadder)
+            while (startPosition != 100)
             {
-                case IS_LADDER:
-                    startPosition += dieRoller;
-                    break;
+                int dieRoller = die.Next(1, 7);
+                Console.WriteLine("Player rolls the die and gets : " + dieRoller);
 
-                case IS_SNAKE:
-                    startPosition -= dieRoller;
-                    break;
+                int SnakeOrLadder = die.Next(0, 3);
+                switch (SnakeOrLadder)
+                {
+                    case IS_LADDER:
+                        Console.WriteLine("Player gets ladder");
+                        startPosition += dieRoller;
+                        break;
 
-                default:                    
-                    break;
-            }            
+                    case IS_SNAKE:
+                        Console.WriteLine("Player gets snake");
+                        startPosition -= dieRoller;
+                        if (startPosition < 0)
+                            startPosition = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("No play.");
+                        break;
+                }
+                Console.WriteLine("Current position : " + startPosition);
+            }
+            Console.WriteLine("Winning position : " + startPosition);
         }
     }
 }
